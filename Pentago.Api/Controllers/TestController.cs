939d563ccfb048916daf30e15f7f0a16b6c92662
baka <dyatlovleonid@gmail.com>
2022-01-12 10:14:24 +0300

    idk

--- Pentago.Api/Controllers/TestController.cs
+++ Pentago.Api/Controllers/TestController.cs
@@ -3,16 +3,19 @@ using Microsoft.AspNetCore.Mvc;
 namespace Pentago.Api.Controllers;
 
 /// <summary>
-/// This controller represents a test endpoint.
+///     This controller represents a test endpoint.
 /// </summary>
 [Route("[controller]")]
 [ApiController]
 public class TestController : ControllerBase
 {
     /// <summary>
-    /// Returns a test payload.
+    ///     Returns a test payload.
     /// </summary>
     /// <returns>A test payload.</returns>
     [HttpGet]
-    public string Get() => "Test endpoint";
+    public string Get()
+    {
+        return "Test endpoint";
+    }
 }
--- Pentago.Services/Engine/Board.cs
+++ Pentago.Services/Engine/Board.cs
@@ -11,7 +11,7 @@ public class Board
             var quad = new Square[9];
             for (var j = 0; j < 9; j++)
                 quad[j] = new Square(j % 3 + i % 2 * 3,
-                    (int) (Math.Floor((float) j / 3) + Math.Floor((float) i / 2) * 3));
+                    (int)(Math.Floor((float)j / 3) + Math.Floor((float)i / 2) * 3));
 
             _board[i] = quad;
         }
@@ -21,7 +21,7 @@ public class Board
     {
         foreach (var square in _board[move.RotateIndex]) square.Rotate(move.Clockwise);
 
-        _board[(int) (Math.Floor((double) move.PlacedPiece.X / 3) + Math.Floor((double) move.PlacedPiece.Y / 3))]
+        _board[(int)(Math.Floor((double)move.PlacedPiece.X / 3) + Math.Floor((double)move.PlacedPiece.Y / 3))]
             [move.PlacedPiece.X % 3 + move.PlacedPiece.Y % 3 * 3].State = move.Color;
 
         return _board;
